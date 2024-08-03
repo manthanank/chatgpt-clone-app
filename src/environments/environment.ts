38b1472a819f8189bf7f24a143359be16ab6376b
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-08-03 21:14:01 +0530

    chore: Update backend package.json scripts and file paths

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
+  apiUrl:
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:3000'
+      : 'https://chatgpt-clone-app-erpk.vercel.app/api',
 };
